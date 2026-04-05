f1d194a164b3cab28b4a58365baaaf65af315c5b
SallyLueNoa <sally.lue.noa@gmail.com>
2026-04-05 23:07:34 +0900

    [Feature implementation] Create a new project. (#1)
    * Copy Gradle configuration files from FRLineAgent.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,57 @@
-val kotlin_version: String by project
-val logback_version: String by project
+/*
+ * Copyright (c) 2026 SallyLueNoa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 plugins {
-    kotlin("jvm") version "2.3.0"
-    id("io.ktor.plugin") version "3.4.2"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ktor)
 }
 
 group = "org.fog_rock"
 version = "0.0.1"
 
-application {
-    mainClass = "io.ktor.server.netty.EngineMain"
-}
-
 kotlin {
     jvmToolchain(21)
 }
 
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
+
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-server-netty")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("io.ktor:ktor-server-core")
-    implementation("io.ktor:ktor-server-config-yaml")
-    testImplementation("io.ktor:ktor-server-test-host")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation(libs.bundles.ktor.server)
+    implementation(libs.bundles.koin)
+    implementation(libs.logback.classic)
+    implementation(libs.google.cloud.secretmanager)
+    implementation(libs.google.api.client)
+    implementation(libs.google.oauth.client.jetty)
+    implementation(libs.google.sheets.api)
+    implementation(libs.bundles.line.bot)
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.kotlin.test.junit)
+    testImplementation(libs.junit.jupiter.api)
+    testImplementation(libs.mockk)
+    testRuntimeOnly(libs.junit.jupiter.engine)
+    testRuntimeOnly(libs.junit.platform.launcher)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.getByName<Jar>("shadowJar") {
+    archiveFileName.set("${project.name}-${project.version}-all.jar")
 }
